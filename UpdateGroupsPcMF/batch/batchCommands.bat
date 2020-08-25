6dc293d8ceacdd689d4b4791f8689036cf7bb8ea
Brian Willis <44345158+BW1ll@users.noreply.github.com>
2020-08-25 11:40:07 -0500

    Added redacted output file

---
+++ UpdateGroupsPcMF/batch/batchCommands.bat
@@ -0,0 +1,171 @@
+@ECHO OFF
+COLOR 0A
+PROMPT MASTER BLASTER
+TITLE DEATH STAR CONSOLE
+
+REM NEED TO HAVE PSEXEC INSTALLED ON LOCAL WINDOWS COMPUTER
+REM HTTPS://DOCS.MICROSOFT.COM/EN-US/SYSINTERNALS/DOWNLOADS/PSEXEC
+REM HTTPS://DOWNLOAD.SYSINTERNALS.COM/FILES/PSTOOLS.ZIP
+REM ADD FILES TO THE THE C:\WINDOWS\SYSTEM32 FOLDER AND ADD
+REM THE LOCATION TO THE "ENVIREMENT VARIABLES" SEE:
+REM HTTPS://WWW.JAVA.COM/EN/DOWNLOAD/HELP/PATH.XML
+
+GOTO :A
+
+:A
+ECHO SETTING WORKING DIR FOR PAPERCUT....
+PAUSE
+CD /D "C:\PROGRAM FILES\PAPERCUT MF\SERVER\BIN\WIN"
+PAUSE 3
+ECHO CD /D "C:\PROGRAM FILES\PAPERCUT MF\SERVER\BIN\WIN"
+CLS
+ECHO READY TO SEND COMMANDS...
+ECHO PRESS ENTER TO CONTINUE...
+PAUSE
+CLS
+
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" TEACHERS_GRP
+server-command set-group-quota TEACHERS_GRP 200.0000 Monthly 100.0000
+server-command add-printer-access-group <server> "Chromebook Pull Print" OFFICE_GRP
+server-command set-group-quota OFFICE_GRP 200.0000 Monthly 100.0000
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+server-command remove-group OU:<REDACTED>OFFICE
+server-command remove-group OU:<REDACTED>TEACHERS
+
+CLS
+ECHO DONE SENDING COMMANDS...
+ECHO PLEASE VERIFY THAT THE COMMANDS HAVE BEEN EXICUTED CORRECTLY
+ECHO PRESS ENTER TO EXITN
+PAUSE
+GOTO END
+:END
